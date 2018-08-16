ad6203c0c5e56c100ef7ffb6c0f6a7e46d14e871
tomof <fffjt007@gmail.com>
2018-08-16 13:52:38 +0900

    modify console.log

--- build.js
+++ build.js
@@ -1,3 +1,3 @@
-console.log(process.env.NODE_ENV)
-console.log(process.env.NODE_VERSION)
-console.log(process.env.NPM_VERSION)
+console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('NODE_VERSION', process.env.NODE_VERSION)
+console.log('NPM_VERSION', process.env.NPM_VERSION)
