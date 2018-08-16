ba544f54e96171c1061e2b20ad39fa3af965a0e2
tomof <fffjt007@gmail.com>
2018-08-16 14:24:57 +0900

    add Intentional error

--- build.js
+++ build.js
@@ -1,3 +1,5 @@
+throw new Error('Intentional Error');
+
 console.log('NODE_ENV', process.env.NODE_ENV)
 console.log('NODE_VERSION', process.env.NODE_VERSION)
 console.log('NPM_VERSION', process.env.NPM_VERSION)
