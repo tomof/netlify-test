a39bcfc76500423e5e47b4c7755c3cf461972cfc
tomof <fffjt007@gmail.com>
2018-08-16 14:53:05 +0900

    fix build process

--- build.js
+++ build.js
@@ -1,5 +1,3 @@
-throw new Error('Intentional Error');
-
 console.log('NODE_ENV', process.env.NODE_ENV)
 console.log('NODE_VERSION', process.env.NODE_VERSION)
 console.log('NPM_VERSION', process.env.NPM_VERSION)
