717ceaab1db4d82ca2677833be3f6103ddd6a86f
tomof <fffjt007@gmail.com>
2018-08-24 15:54:21 +0900

    add console.log

--- log.js
+++ log.js
@@ -1,4 +1,6 @@
 console.log('NODE_ENV', process.env.NODE_ENV)
+console.log('BRANCH', process.env.BRANCH)
+console.log('CONTEXT', process.env.CONTEXT)
 console.log('NODE_VERSION', process.env.NODE_VERSION)
 console.log('NPM_VERSION', process.env.NPM_VERSION)
 console.log('REPOSITORY_URL', process.env.REPOSITORY_URL)
